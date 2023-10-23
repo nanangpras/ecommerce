dade31c618e917c9679dcf720a4bd51b0c5824a8
nanangpras <prasetiyantara@gmail.com>
2023-10-23 18:16:02 +0700

    update menu mobile

--- resources/views/home/includes/topnavbar-mobile.blade.php
+++ resources/views/home/includes/topnavbar-mobile.blade.php
@@ -2,7 +2,7 @@
     <div class="ltn__utilize-menu-inner ltn__scrollbar">
         <div class="ltn__utilize-menu-head">
             <div class="site-logo">
-                <a href="index.html"><img src="{{url('themes-frontend/img/logo.png')}}" alt="Logo"></a>
+                <a href="{{route('home')}}"><img src="{{url('themes-frontend/img/logo.png')}}" alt="Logo"></a>
             </div>
             <button class="ltn__utilize-close">×</button>
         </div>
@@ -14,83 +14,29 @@
         </div>
         <div class="ltn__utilize-menu">
             <ul>
-                <li><a href="#">Home</a>
-                    <ul class="sub-menu">
-                        <li><a href="index.html">Home Style 01</a></li>
-                        <li><a href="index-2.html">Home Style 02</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">About</a>
-                    <ul class="sub-menu">
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="service.html">Services</a></li>
-                        <li><a href="service-details.html">Service Details</a></li>
-                        <li><a href="portfolio.html">Portfolio</a></li>
-                        <li><a href="portfolio-2.html">Portfolio - 02</a></li>
-                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                        <li><a href="team.html">Team</a></li>
-                        <li><a href="team-details.html">Team Details</a></li>
-                        <li><a href="faq.html">FAQ</a></li>
-                        <li><a href="locations.html">Google Map Locations</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Shop</a>
-                    <ul class="sub-menu">
-                        <li><a href="shop.html">Shop</a></li>
-                        <li><a href="shop-grid.html">Shop Grid</a></li>
-                        <li><a href="shop-left-sidebar.html">Shop Left sidebar</a></li>
-                        <li><a href="shop-right-sidebar.html">Shop right sidebar</a></li>
-                        <li><a href="product-details.html">Shop details </a></li>
-                        <li><a href="cart.html">Cart</a></li>
-                        <li><a href="wishlist.html">Wishlist</a></li>
-                        <li><a href="checkout.html">Checkout</a></li>
-                        <li><a href="order-tracking.html">Order Tracking</a></li>
-                        <li><a href="account.html">My Account</a></li>
-                        <li><a href="login.html">Sign in</a></li>
-                        <li><a href="register.html">Register</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">News</a>
-                    <ul class="sub-menu">
-                        <li><a href="blog.html">News</a></li>
-                        <li><a href="blog-grid.html">News Grid</a></li>
-                        <li><a href="blog-left-sidebar.html">News Left sidebar</a></li>
-                        <li><a href="blog-right-sidebar.html">News Right sidebar</a></li>
-                        <li><a href="blog-details.html">News details</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">Pages</a>
-                    <ul class="sub-menu">
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="service.html">Services</a></li>
-                        <li><a href="service-details.html">Service Details</a></li>
-                        <li><a href="portfolio.html">Portfolio</a></li>
-                        <li><a href="portfolio-2.html">Portfolio - 02</a></li>
-                        <li><a href="portfolio-details.html">Portfolio Details</a></li>
-                        <li><a href="team.html">Team</a></li>
-                        <li><a href="team-details.html">Team Details</a></li>
-                        <li><a href="faq.html">FAQ</a></li>
-                        <li><a href="history.html">History</a></li>
-                        <li><a href="add-listing.html">Add Listing</a></li>
-                        <li><a href="locations.html">Google Map Locations</a></li>
-                        <li><a href="404.html">404</a></li>
-                        <li><a href="contact.html">Contact</a></li>
-                        <li><a href="coming-soon.html">Coming Soon</a></li>
-                    </ul>
-                </li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><a href="{{route('home')}}">Home</a></li>
+                <li><a href="{{route('home.about')}}">About</a></li>
+                <li><a href="{{route('shop.index')}}">Shop</a></li>
+                <li><a href="{{route('home')}}">News</a></li>
+                <li><a href="{{route('home')}}">Contact</a></li>
             </ul>
         </div>
         <div class="ltn__utilize-buttons ltn__utilize-buttons-2">
             <ul>
-                <li>
-                    <a href="account.html" title="My Account">
-                        <span class="utilize-btn-icon">
-                            <i class="far fa-user"></i>
-                        </span>
-                        My Account
-                    </a>
-                </li>
+                @guest
+                    <li><a href="{{route('login.user')}}">Sign in</a></li>
+                    <li><a href="{{route('register.user')}}">Register</a></li>
+                @endguest
+                @auth
+                    <li>
+                        <a href="{{route('member.dashboard')}}" title="My Account">
+                            <span class="utilize-btn-icon">
+                                <i class="far fa-user"></i>
+                            </span>
+                            My Account
+                        </a>
+                    </li>
+                @endauth
                 <li>
                     <a href="wishlist.html" title="Wishlist">
                         <span class="utilize-btn-icon">
@@ -101,7 +47,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="cart.html" title="Shoping Cart">
+                    <a href="{{route('cart.index')}}" title="Shoping Cart">
                         <span class="utilize-btn-icon">
                             <i class="fas fa-shopping-cart"></i>
                             <sup>5</sup>
