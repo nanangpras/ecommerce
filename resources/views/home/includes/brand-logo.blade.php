d2a1568e6dc3a36bd3d9bfed3d82ce6cfdd1290b
rifqiwar <r.wariyanto@gmail.com>
2023-11-03 13:10:54 +0700

    logo hilang

--- resources/views/home/includes/brand-logo.blade.php
+++ resources/views/home/includes/brand-logo.blade.php
@@ -1,6 +1,7 @@
 <div class="ltn__brand-logo-area ltn__brand-logo-1 before-bg-bottom">
     <div class="container">
-        <div class="row ltn__brand-logo-active ltn__secondary-bg ltn__border-radius pt-30 pb-20">
+
+        {{-- <div class="row ltn__brand-logo-active ltn__secondary-bg ltn__border-radius pt-30 pb-20">
             <div class="col-lg-12">
                 <div class="ltn__brand-logo-item">
                     <img src="{{url('themes-frontend/img/brand-logo/b11.png')}}" alt="Brand Logo">
@@ -31,6 +32,6 @@
                     <img src="{{url('themes-frontend/img/brand-logo/b13.png')}}" alt="Brand Logo">
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </div>
