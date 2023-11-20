171d691ddee81cfc8478ec7242febb3d7953dc3b
nangpras <nang.prasen@gmail.com>
2023-11-20 19:42:19 +0700

    delete cart top bar

--- resources/views/home/layouts/layout-pages.blade.php
+++ resources/views/home/layouts/layout-pages.blade.php
@@ -8,6 +8,8 @@
     <meta name="robots" content="noindex, follow" />
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     @include('home.includes.style')
 </head>
 
