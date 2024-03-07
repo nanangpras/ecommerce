bc75b6d8d5a36f0740d3a469ce97762c8b3fc4a4
Arifsetyaji101 <Arifsetyaji101@gmail.com>
2024-03-07 09:03:06 +0700

    update - icon product homepage

--- resources/views/home/homefront.blade.php
+++ resources/views/home/homefront.blade.php
@@ -269,12 +269,64 @@ class="{{ $key == 0 ? 'active' : ' ' }}"></li>
                                 text-overflow: ellipsis;">{{$item->title}}</h4>
                             </a>
                             <p class="text-start m-0">{{$item->category->name ?? ''}}</p>
+
+                            <!-- Button Preview & Info -->
+                            <div class="d-flex float-end align-items-center">
+                                <a href="{{route('product.detail',$item->slug)}}" class="group flex relative">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                        fill="url(#customGradient)" class="w-8 h-8 svg-icon-product">
+                                        <path fill-rule="evenodd"
+                                            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                                            clip-rule="evenodd" />
+                                        <defs>
+                                            <linearGradient id="customGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                                <stop offset="0%" style="stop-color: #1488c9" />
+                                                <stop offset="48%" style="stop-color: #5fa7d0" />
+                                                <stop offset="100%" style="stop-color: #669bda" />
+                                            </linearGradient>
+
+                                            <linearGradient id="hoverGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                                <stop offset="0%" style="stop-color: #4797b0" />
+                                                <stop offset="48%" style="stop-color: #4da9bd" />
+                                                <stop offset="100%" style="stop-color: #4dbd92" />
+                                            </linearGradient>
+                                        </defs>
+                                    </svg>
+                                    <span class="group-hover:opacity-100 transition-opacity bg-gray-800 p-2 text-sm text-gray-100 rounded-md absolute left-1/2 
+                                     -translate-x-1/2 translate-y-full opacity-0 mx-auto">Detail</span>
+                                </a>
+
+                                @if ($item->link)
+                                <div class="ms-2">
+                                    <a href="{{$item->link}}" target="_blank" class="group flex relative">
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                                            fill="url(#customGradient)" class="w-8 h-8 svg-icon-product">
+                                            <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                                            <path fill-rule="evenodd"
+                                                d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                                                clip-rule="evenodd" />
+                                            <defs>
+                                                <linearGradient id="customGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                                    <stop offset="0%" style="stop-color: #1488c9" />
+                                                    <stop offset="48%" style="stop-color: #5fa7d0" />
+                                                    <stop offset="100%" style="stop-color: #669bda" />
+                                                </linearGradient>
+
+                                                <linearGradient id="hoverGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                                                    <stop offset="0%" style="stop-color: #4797b0" />
+                                                    <stop offset="48%" style="stop-color: #4da9bd" />
+                                                    <stop offset="100%" style="stop-color: #4dbd92" />
+                                                </linearGradient>
+                                            </defs>
+                                        </svg>
+                                        <span class="group-hover:opacity-100 transition-opacity bg-gray-800 p-2 text-sm text-gray-100 rounded-md absolute left-1/2 
+                                         -translate-x-1/2 translate-y-full opacity-0 mx-auto">Preview</span>
+                                    </a>
+                                </div>
+                                @endif
+                            </div>
+                            <!-- END Button Preview & Info -->
                         </div>
-                    </div>
-                    <div class="float-end ">
-                        <a href="{{$item->link}}" target="_blank" class="btn-product me-3">
-                            Demo
-                        </a>
                     </div>
                 </div>
             </div>
@@ -669,7 +721,8 @@ class="{{ $key == 0 ? 'active' : ' ' }}"></li>
                         </div>
                     </div>
                     <p>
-                        Website bagus, banyak templatenya yang bisa dipilih. Harga termasuk murah karena sudah paket lengkap.
+                        Website bagus, banyak templatenya yang bisa dipilih. Harga termasuk murah karena sudah paket
+                        lengkap.
                     </p>
                 </div>
             </div>
